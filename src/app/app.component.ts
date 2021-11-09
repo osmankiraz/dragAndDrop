6f74b111e6af194d542e7b85988ff35735fb4aae
Osman Kiraz <osmnkiraz11@gmail.com>
2021-11-09 10:23:22 +0300

    initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from "@angular/cdk/drag-drop";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dragAndDrop';
+  numbers: number[] = []
+  orherNumbers:number[]=[];
+
+
+  constructor() {
+    for (let i = 0; i < 10; i++) {
+      this.numbers.push(i)
+    }
+  }
+
+  ngOnInit(): void {
+  }
+
+
+  drop(event: CdkDragDrop<number[]>) {
+
+    if (event.previousContainer !== event.container){
+      transferArrayItem(event.previousContainer.data,event.container.data,event.previousIndex,event.currentIndex)
+    }else {
+     moveItemInArray(this.numbers,event.previousIndex,event.currentIndex)
+    }
+     console.log('event yazdır => ', event )
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
